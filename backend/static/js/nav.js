8fa3bb36d19f49b2f37a01223bd98acf743aa381
Marce Elizeche <melizeche@gmail.com>
2015-02-21 00:30:20 -0300

    Navegar la lista como texto

--- backend/static/js/nav.js
+++ backend/static/js/nav.js
@@ -1,17 +1,49 @@
-load_posts("/api/v1/denuncias/");
 
 var next="";
 var cant=0;
 var total=0;
+var elements=[];
+var flag=true;
+
+//$("#tabla").hide();
+
+load_posts("/api/v1/denuncias/");
+
 
 function convert_to_readable_date(date_time_string) {
-    var newDate = moment(date_time_string).format('MM/DD/YYYY, h:mm:ss a')
+    var newDate = moment(date_time_string).format('DD/MM/YYYY, h:mm:ss a')
     return newDate
 }
 // Load all posts on page load
+function hideDivs () {
+    if(flag){
+        $("#vis").html("Ver como  galeria");
+        $("#denuncias").hide();
+        $("#tabla").show();
+        
+    }else{
+        $("#vis").html("Ver como lista");
+        $("#denuncias").show();
+        $("#tabla").hide();
+    }
+    flag=!flag;
+
+    // body...
+}
+
+function toTable(){
+    var par="";
+    //$("#denuncias").toggle();
+    $("#tb").html("");
+      for (var i = 0; i < elements.length; i++) {
+        var par = ( i & 1 ) ? "pure-table-odd" : "";
+        var desc = ( elements[i].desc ) ? elements[i].desc : "";
+        $("#tb").append("<tr class='"+par+"' ><td>"+(i+1)+"</td><td>"+elements[i].numero+" </td><td> "+elements[i].tipo+" </td> <td> "+desc+" </td></tr>");
+      }
+    //$("#denuncias").append("</tbody></table>");
+}
 
 function load_posts(url) {
-	console.log("hola");
     $.ajax({
         url : url, // the endpoint
         type : "GET", // http method
@@ -23,16 +55,19 @@ function load_posts(url) {
         	//console.log(json.next);
         	cant = cant + results.length;
         	next=json.next;
+            var desc="";
             for (var i = 0; i < results.length; i++) {
                 //console.log(results[i]);
+
+                elements.push(results[i]);
                 dateString = convert_to_readable_date(results[i].added);
-                if(! results[i].desc){ results[i].desc=results[i].numero;}
+                if(! results[i].desc){ desc=results[i].numero;}else{desc=results[i].desc;}
                 if(screen.width<768){
                 	$("#denuncias").append('<div class="pure-u-1-2" style="margin-right:1em" title="Denuncia #'+results[i].id+'"> <figure><label>Número:'+results[i].numero+'</label> <label> Tipo:'+results[i].tipo+'</label> <a href="'+results[i].screenshot+'" target="_blank"><img src="'+results[i].screenshot+'"></a> <figcaption>'+ dateString+'</figcaption></figure>');	
                 }else{
                 	$("#denuncias").append('<div class="pure-u-1-4" style="margin-right:1em" title="Denuncia #'+results[i].id+'"> <figure><label>Número:'+results[i].numero+'</label> <label> Tipo:'+results[i].tipo+'</label> <a href="'+results[i].screenshot+'" target="_blank"><img src="'+results[i].screenshot+'"></a> <figcaption>'+ dateString+'</figcaption></figure>');
             }	}
-            console.log(next);
+            toTable();
             $("#canti").html("Se obtuvieron " +cant+" de "+ total + " denuncias");
             if (!next){
             	//console.log('aaaa');
